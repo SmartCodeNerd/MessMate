5f1d6a8334dbe32e7effaa7842c35b52b5c5e102
Goldie2311 <coding.messiah007@gmail.com>
2025-08-02 01:50:12 +0530

    commit

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,7 +1,4 @@
 import express from "express"
 const router = express.Router();
-import {createUser} from "../controllers/userController.js";
-
-router.post("/create-user",protect,createUser);
 
 export default router;
