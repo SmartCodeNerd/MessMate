9e2bf1653d18237cd6b18caf6eb77bccf6008808
Goldie2311 <coding.messiah007@gmail.com>
2025-08-01 23:39:00 +0530

    coupon model done

---
+++ Backend/src/models/couponModel.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const DailyMealSchema = new mongoose.Schema({
+    date: {
+        type: String, // Format: YYYY-MM-DD
+        required: true,
+    },
+    breakfast: {
+        type: Boolean,
+        default: false,
+    },
+    lunch: {
+        type: Boolean,
+        default: false,
+    },
+    dinner: {
+        type: Boolean,
+        default: false,
+    }
+    }, { _id: false });
+
+    const CouponSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    weekStartDate: {
+        type: String, // YYYY-MM-DD
+        required: true,
+    },
+    weekEndDate: {
+        type: String, // YYYY-MM-DD
+        required: true,
+    },
+    meals: {
+        type: [DailyMealSchema],
+        required: true,
+        validate: v => v.length > 0,
+    },
+    totalAmount: {
+        type: Number,
+        required: true,
+        min: 432,
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['paid', 'pending', 'failed'],
+        default: 'pending',
+    },
+    }, {
+    timestamps: true,
+    });
+
+    module.exports = mongoose.model('Coupon', CouponSchema);
