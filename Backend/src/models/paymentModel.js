6028c876a9ca8fd72dce40b1ca75fdd9ce31608b
sashi17 <sashikantapattanaik2005@gmail.com>
2025-10-30 02:01:55 +0530

    updates

--- Backend/src/models/paymentModel.js
+++ Backend/src/models/paymentModel.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
 const paymentSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
+    
   amount: {
     type: Number,
     required: true,
